b507484abe37d4f55bdbe542825f663583296ee5
princesh14 <76401356+princesh14@users.noreply.github.com>
2021-08-04 15:33:24 +0530

    Update SocketServer.java

--- SocketServer.java
+++ SocketServer.java
@@ -1,75 +1,78 @@
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.*;
-import java.io.*;
 
-public class ClientSide {
+public class ServerSide {
 
-        Socket socket;
-        BufferedReader br;
-        PrintWriter out;
+    ServerSocket server;
+    Socket socket;
+    BufferedReader br;
+    PrintWriter out;
+     public ServerSide(){
+try{
+    server = new ServerSocket(7777);
+    System.out.println("server is ready to accept a connection");
+    System.out.println("waiting");
+    socket=server.accept();
+    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new PrintWriter(socket.getOutputStream());
 
-    public ClientSide() {
-        try {
-            System.out.println("Sending request to server!!!");
-            socket = new Socket("127.0.0.1", 7777);
-            System.out.println("Connection Done!!!");
-
-            br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            out = new PrintWriter(socket.getOutputStream());
-
-            startReading();
-            startWriting();
-        } catch (Exception e) {
-            //
-        }
-    }
-        public void startReading() {
-
-            Runnable r1 = () -> {
-                System.out.println("Reader Started");
-                while (true) {
-                    try {
-                        String msg = br.readLine();
-                        if (msg.equals("exit")) {
-                            System.out.println("Server terminated the chat");
-
-                            break;
-                        }
-                        System.out.println("Server : " + msg);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            };
-            new Thread(r1).start();
-
-        }
-        public void startWriting () {
-
-            Runnable r2 = () -> {
-                    System.out.println("writer started");
-                while (true) {
-                    try {
-                        BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
-                        String content = br1.readLine();
-                        out.println(content);
-                        out.flush();
-
-                    } catch (Exception e) {
-                       e.printStackTrace();
-                    }
-                }
-            };
-            new Thread(r2).start();
-
-        }
-        public static void main (String[]args){
-
-            System.out.println("This is Client");
-            new ClientSide();
-        }
-
+    startReading();
+    startWriting();
 }
+catch (Exception e){
+    e.printStackTrace();
+    }
+     }
+     public void startReading(){
+
+         Runnable r1 = ()->{
+             System.out.println("Reader Started");
+             while (true){
+                 try {
+                     String msg = br.readLine();
+                     if (msg.equals("exit")) {
+                         System.out.println("Client terminated the chat");
+
+                         break;
+                     }
+                     System.out.println("Client : " + msg);
+                 }
+                 catch (Exception e){
+                     e.printStackTrace();
+                 }
+             }
+
+         };
+         new Thread(r1).start();
+
+     }
+     public void startWriting(){
+         Runnable r2 = () -> {
+             System.out.println("writer started");
+
+             while (true){
+                 try{
+               BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                String content = br1.readLine();
+                out.println(content);
+                out.flush();
+
+                 }
+                 catch (Exception e){
+                     e.printStackTrace();
+                 }
+             }
+         };
+         new Thread(r2).start();
+
+     }
 
 
+    public static void main(String[] args) {
+        System.out.println("This is Server.. Going to start a server");
+        new ServerSide();
+
+    }
+}
