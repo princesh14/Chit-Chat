0ae70ebb7020c3af5506c4eaf45172b040eaa953
princesh14 <76401356+princesh14@users.noreply.github.com>
2021-08-04 15:31:24 +0530

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -1,109 +1,80 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
+import java.net.*;
 
-@SuppressWarnings("serial")
-public class SocketClient extends JFrame implements ActionListener, Runnable {
-    JTextArea textArea = new JTextArea();
-    JScrollPane jp = new JScrollPane(textArea);
-    JTextField input_Text = new JTextField();
-    JMenuBar menuBar = new JMenuBar();
+public class ServerSide {
 
-    Socket sk;
+    ServerSocket server;
+    Socket socket;
     BufferedReader br;
-    PrintWriter pw;
+    PrintWriter out;
+     public ServerSide(){
+try{
+    server = new ServerSocket(7777);
+    System.out.println("server is ready to accept a connection");
+    System.out.println("waiting");
+    socket=server.accept();
+    br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    out = new PrintWriter(socket.getOutputStream());
 
-    public SocketClient() {
-        super("Joto Iccha Chat Koro");
-        setFont(new Font("Arial Black", Font.PLAIN, 12));
-        setForeground(new Color(0, 0, 51));
-        setBackground(new Color(51, 0, 0));
-        textArea.setToolTipText("Chat History");
-        textArea.setForeground(new Color(50, 205, 50));
-        textArea.setEditable(false);
-        textArea.setFont(new Font("Monospaced", Font.BOLD, 13));
-
-        textArea.setBackground(new Color(0, 0, 0));
-
-/*
-         JMenu helpMenu = new JMenu("Help");
-        JMenuItem update = new JMenuItem("Update Information");
-        JMenuItem connect_List = new JMenuItem("Visitor List");
-
-        helpMenu.add(update);
-        helpMenu.add(connect_List);
-
-        menuBar.add(helpMenu);
-        setJMenuBar(menuBar);
-*/
-        getContentPane().add(jp, "Center");
-        input_Text.setText("Enter your Message:");
-        input_Text.setToolTipText("Enter your Message");
-        input_Text.setForeground(new Color(0, 0, 0));
-        input_Text.setFont(new Font("Tahoma", Font.BOLD, 11));
-        input_Text.setBackground(new Color(230, 230, 250));
-        
-        getContentPane().add(input_Text, "South");
-        setSize(325, 411);
-        setVisible(true);
-
-        input_Text.requestFocus(); //Place cursor at run time, work after screen is shown
-
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        input_Text.addActionListener(this); //Event registration
+    startReading();
+    startWriting();
+}
+catch (Exception e){
+    e.printStackTrace();
     }
+     }
+     public void startReading(){
 
-    public void serverConnection() {
-        try {
-            String IP = JOptionPane.showInputDialog(this, "Please enter a server IP.", JOptionPane.INFORMATION_MESSAGE);
-            sk = new Socket(IP, 1234);
+         Runnable r1 = ()->{
+             System.out.println("Reader Started");
+             while (true){
+                 try {
+                     String msg = br.readLine();
+                     if (msg.equals("exit")) {
+                         System.out.println("Client terminated the chat");
 
-            String name = JOptionPane.showInputDialog(this, "Please enter a nickname", JOptionPane.INFORMATION_MESSAGE);
-/*            while (name.length() > 7) {
-                name = JOptionPane.showInputDialog(this, "Please enter a nickname.(7 characters or less)", JOptionPane.INFORMATION_MESSAGE);
-            }
-*/
-            //read
-            br = new BufferedReader(new InputStreamReader(sk.getInputStream()));
+                         break;
+                     }
+                     System.out.println("Client : " + msg);
+                 }
+                 catch (Exception e){
+                     e.printStackTrace();
+                 }
+             }
 
-            //writing
-            pw = new PrintWriter(sk.getOutputStream(), true);
-            pw.println(name); // Send to server side
+         };
+         new Thread(r1).start();
 
-            new Thread(this).start();
+     }
+     public void startWriting(){
+         Runnable r2 = () -> {
+             System.out.println("writer started");
 
-        } catch (Exception e) {
-            System.out.println(e + " Socket Connection error");
-        }
-    }
+             while (true){
+                 try{
+               BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+                String content = br1.readLine();
+                out.println(content);
+                out.flush();
+
+                 }
+                 catch (Exception e){
+                     e.printStackTrace();
+                 }
+             }
+         };
+         new Thread(r2).start();
+
+     }
+
 
     public static void main(String[] args) {
-        new SocketClient().serverConnection(); //Method call at the same time object creation
-    }
+        System.out.println("This is Server.. Going to start a server");
+        new ServerSide();
 
-    @Override
-    public void run() {
-        String data = null;
-        try {
-            while ((data = br.readLine()) != null) {
-                textArea.append(data + "\n"); //textArea Decrease the position of the box's scroll bar by the length of the text entered
-                textArea.setCaretPosition(textArea.getText().length());
-            }
-        } catch (Exception e) {
-            System.out.println(e + "--> Client run fail");
-        }
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        String data = input_Text.getText();
-        pw.println(data); // Send to server side
-        input_Text.setText("");
     }
 }
